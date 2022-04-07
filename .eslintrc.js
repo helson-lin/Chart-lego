6add3528dde2ca793b7193356fd3a29e47014807
hejianglin <hejianglin@uino.cn>
2022-04-07 14:57:16 +0800

    feat: init base file for project

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/typescript/recommended",
+    "plugin:prettier/recommended",
+  ],
+  plugins: ["prettier"],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {
+    "prettier/prettier": "error",
+    "arrow-body-style": "off",
+    "prefer-arrow-callback": "off",
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    semi: [1, "never"],
+  },
+};
